2a6284769379f9662a2b4812fb4ad6356327d33c
moosarehan <moosarehan4@gmail.com>
2026-03-08 16:59:07 +0500

    INPUT/OUTPUT IMAGES IN OPENCV

--- io_img.py
+++ io_img.py
@@ -1,9 +1,9 @@
 import cv2
 #reading an image from  local disk
-img = cv2.imread('image/istockphoto-627966690-612x612.jpg')
+img = cv2.imread('images/istockphoto-627966690-612x612.jpg')
 
 # writing the image to the local dist
-cv2.imwrite('image/outout.jpg',img)
+cv2.imwrite('images/outout.jpg',img)
 
 #displaying an image
 cv2.imshow('FRAME',img)
