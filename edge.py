09b6199ced86a7895f5dbbe6330d23342d959127
moosarehan <moosarehan4@gmail.com>
2026-03-08 17:05:11 +0500

    EDGE DETECTION

---
+++ edge.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+img = cv2.imread('images/istockphoto-627966690-612x612.jpg')
+
+edge=cv2.Canny(img,100,200)
+kernel = np.ones((3, 3), dtype=np.uint8)
+edge_d=cv2.dilate(edge,kernel)
+edge_e=cv2.erode(edge_d,kernel)
+cv2.imshow('edge',edge)
+cv2.imshow('dilate',edge_d)
+cv2.imshow('erode',edge_e)
+cv2.waitKey(0)
+
